2b34c866886e7c0ce5894022e288780e8fbd26af
evan.loria4 <evan.loria4@gmail.com>
2025-01-17 16:23:04 -0600

    Build the webpack config object

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const SRC_DIR = path.resolve(__dirname, 'src', 'client' );
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(SRC_DIR, 'index.tsx'),
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript']
+            }
+        },
+      ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          'postcss-loader'
+        ],
+      },
+      // {
+      //   test: /\.(ts|tsx)$/,
+      //   use: 'ts-loader',
+      //   exclude: /node_modules/
+      // }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(SRC_DIR, 'index.html'),
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ]
+  }
+}
