24ce89dd33cdc76fe1cfa8830aa74d46937b2c8a
coltongraygg <coltongraygg@gmail.com>
2025-01-18 15:12:21 -0600

    SETUP ssl/https

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,19 @@
 const { app } = require('./app.ts')
-
+const https = require('https');
 const PORT = 4000;
+const fs = require('fs');
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Listening on http://localhost:${PORT}`);
-});
+
+if (process.env.DEVELOPMENT === 'true') {
+    app.listen(PORT, '0.0.0.0', () => {
+        console.log(`Listening on http://localhost:${PORT}`);
+    });
+} else {
+    const options = {
+        cert: fs.readFileSync('/etc/letsencrypt/live/slayer.events/fullchain.pem'),
+        key: fs.readFileSync('/etc/letsencrypt/live/slayer.events/privkey.pem'),
+    }
+    https.createServer(options, app).listen(443);
+}
+
 
