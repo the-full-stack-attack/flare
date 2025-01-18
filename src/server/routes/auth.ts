d826abb6da9b89d2d2398f5bae4543bd520f5218
coltongraygg <coltongraygg@gmail.com>
2025-01-17 19:21:21 -0600

    ADD passport google oauth setup

--- src/server/routes/auth.ts
+++ src/server/routes/auth.ts
@@ -1,3 +1,10 @@
 const express = require('express');
 const authRouter = express.router();
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const passport = require('passport');
 
+
+
+passport.initialize();
+authRouter.use(passport.session())
+app.use(new GoogleStrategy)
