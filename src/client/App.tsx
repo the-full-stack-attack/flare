09d563dc3776df96252bb51eb525d785f41cf1dc
evan.loria4 <evan.loria4@gmail.com>
2025-01-18 14:12:19 -0600

    Import Home view to add as a route

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-dom';
-import { Home } from './views/Home';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Home from './views/Home';
 
 
 
