7ac4e5374158f0f7e324e1ce32daa1f71fcf7668
bkhebert <bondman2004tippy@gmail.com>
2025-01-18 19:44:17 -0600

    ADD Users table to models, exported into index

---
+++ src/server/db/models/users.ts
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../index');
+
+const Users = sequelize.define('Users', {
+  user_id:{
+    type:Sequelize.INTEGER,
+    autoIncrement:true,
+    allowNull:false,
+    primaryKey:true,
+  },
+  username: { type: Sequelize.STRING(20) },
+  email: { type: Sequelize.STRING } ,
+  full_name: { type: Sequelize.STRING },
+  phone_number: { type: Sequelize.STRING(10) },
+  tasks_completed: { type: Sequelize.INTEGER },
+  events_attended: {type: Sequelize.INTEGER },
+  location: { type: Sequelize.STRING },
+  avatar_shirt: { type: Sequelize.STRING },
+  avatar_pants: { type: Sequelize.STRING },
+
+})
+
+module.exports = {
+  Users
+}
