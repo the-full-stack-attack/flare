84b4383119407349d2705a8584840fb815c07b42
bkhebert <bondman2004tippy@gmail.com>
2025-01-20 11:14:01 -0600

    ADD post request handler to signup router

--- src/server/routes/signup.ts
+++ src/server/routes/signup.ts
@@ -1,6 +1,13 @@
 const { Router } = require('express');
 const signUpRouter = Router();
 
+  signUpRouter.post('/', (req: any, res: any ) => {
+
+    console.log(`post req to '/' received by signup \n body is ${req.body}`)
+    res.sendStatus(201);
+    
+  } )
+
 module.exports = {
   signUpRouter
 };
