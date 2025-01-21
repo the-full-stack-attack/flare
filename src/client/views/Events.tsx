1df77b0b6b47e82b7e4c11a090441eb817965010
tymey <tyler.meyer.a@gmail.com>
2025-01-21 14:55:22 -0600

    refactor: Events view follows lint rules

--- src/client/views/Events.tsx
+++ src/client/views/Events.tsx
@@ -1,24 +1,39 @@
 import React, { useEffect, useState } from 'react';
 
+/////
+
+type GeoPosition = {
+  coords: {
+    latitude: number;
+    longitude: number;
+  };
+};
+
+type Location = {
+  latitude: null | number;
+  longitude: null | number;
+};
+
 function Events() {
-  const [location, setLocation] = useState({ latitude: null, longitude: null });
+  const [location, setLocation] = useState<Location>({
+    latitude: 0,
+    longitude: 0,
+  });
 
   const getGeoLocation = () => {
-    const success = (position: any) => {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
+    const success = (position: GeoPosition) => {
+      const { latitude, longitude } = position.coords;
 
       setLocation({ latitude, longitude });
-    }
+    };
 
     const error = () => {
-      console.error("Unable to retrieve your location");
+      console.error('Unable to retrieve your location');
     };
 
     if (!navigator.geolocation) {
-      console.error("Geolocation is not supported by your browser");
+      console.error('Geolocation is not supported by your browser');
     } else {
-      console.log("Locating…");
       navigator.geolocation.getCurrentPosition(success, error);
     }
   };
@@ -27,13 +42,11 @@ function Events() {
     getGeoLocation();
   }, []);
 
-  console.log(location);
-
   return (
     <div>
       <h4>Hello Stanky</h4>
     </div>
-  )
-};
+  );
+}
 
 export default Events;
