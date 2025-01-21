214dc976bd829f72816f6a36239c15c70a7cde46
coltongraygg <coltongraygg@gmail.com>
2025-01-21 13:32:18 -0600

    merging

--- src/server/routes/event.ts
+++ src/server/routes/event.ts
@@ -1,7 +1,25 @@
 const { Router } = require('express');
-
+const { Event } = require('../db/models/index.ts');
 const eventRouter = Router();
 // Coltron
+
+eventRouter.post('/', async (req: unknown, res: unknown ) => {
+  const { username, fullName, phone, address, selectedInterests, category, startDate, endDate, }
+  console.log('Post req to / received by eventRouter');
+  try {
+    const newEvent = await Event.create({
+      username: username,
+      full_name: fullName,
+      phone_number: phone,
+      address: address,
+      interests: selectedInterests,
+      category: category,
+      stardate: startDate,
+      endDate: endDate,
+    })
+  }
+})
+
 module.exports = {
   eventRouter,
 };
