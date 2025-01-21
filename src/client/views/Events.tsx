1d22d42d82d83f7a3c8a1d3ea41302ee36d604fa
tymey <tyler.meyer.a@gmail.com>
2025-01-20 19:56:12 -0600

    feat: App asked for user location when visiting Events view

--- src/client/views/Events.tsx
+++ src/client/views/Events.tsx
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Events() {
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
+
+  const getGeoLocation = () => {
+    const success = (position: any) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      setLocation({ latitude, longitude });
+    }
+
+    const error = () => {
+      console.error("Unable to retrieve your location");
+    };
+
+    if (!navigator.geolocation) {
+      console.error("Geolocation is not supported by your browser");
+    } else {
+      console.log("Locating…");
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  };
+
+  useEffect(() => {
+    getGeoLocation();
+  }, []);
+
+  console.log(location);
+
   return (
     <div>
       <h4>Hello Stanky</h4>
