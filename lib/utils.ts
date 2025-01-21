4305762838ae375302904865eba51c571245b04c
bkhebert <bondman2004tippy@gmail.com>
2025-01-20 23:56:49 -0600

    ADD components.json for Magic UI

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,4 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+// clsx allows constructing classnames conditionally, needed for magic to work
