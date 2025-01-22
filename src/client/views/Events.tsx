839b0808f8b18f7764ee4e5e018aae262638d474
tymey <tyler.meyer.a@gmail.com>
2025-01-21 19:19:02 -0600

    feat: Retrieve events on Events view mount

--- src/client/views/Events.tsx
+++ src/client/views/Events.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
-/////
+import axios from 'axios';
 
 type GeoPosition = {
   coords: {
@@ -14,11 +13,29 @@ type Location = {
   longitude: null | number;
 };
 
+type EventData = {
+  id: number;
+  title: string;
+  start_time: Date;
+  end_time: Date;
+  address: string;
+  description: string;
+  venue_id: number;
+  created_by: number;
+  chatroom_id: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
 function Events() {
+  // The latitude and longitude of the user will be stored in state on page load
   const [location, setLocation] = useState<Location>({
     latitude: 0,
     longitude: 0,
   });
+
+  // Events the user can attend will be stored in state on page load
+  const [events, setEvents] = useState<EventData[]>([]);
 
   const getGeoLocation = () => {
     const success = (position: GeoPosition) => {
@@ -38,13 +55,34 @@ function Events() {
     }
   };
 
+  const getEvents = () => {
+    axios
+      .get('/event')
+      .then(({ data }) => {
+        setEvents(data);
+      })
+      .catch((err: unknown) => {
+        console.error('Failed to getEvents:', err);
+      });
+  };
+
   useEffect(() => {
     getGeoLocation();
+    getEvents();
   }, []);
 
   return (
-    <div>
-      <h4>Hello Stanky</h4>
+    <div className="container mx-auto px-4 content-center">
+      <h1 className="text-2xl font-bold text-center">Events In Your Area</h1>
+      <ul className="container content-center">
+        {events.map((event: EventData) => (
+          <li key={event.id} className="text-lg text-center">
+            <p>{event.title}</p>
+            <button type="button">Attend</button>
+          </li>
+        ))}
+      </ul>
+      <h1 className="text-2xl font-bold text-center">Events Attending</h1>
     </div>
   );
 }
