dc64893eed91b3baca38d78a9a322b9c24b53b23
evan.loria4 <evan.loria4@gmail.com>
2025-01-22 16:47:19 -0600

    Change User table to uses DataTypes instead of Sequelize

--- src/server/db/models/users.ts
+++ src/server/db/models/users.ts
@@ -1,18 +1,18 @@
-import Sequelize from 'sequelize';
+import { DataTypes } from 'sequelize';
 import database from '../index';
 
 const User = database.define('User', {
-  username: { type: Sequelize.STRING(20) },
-  google_id: { type: Sequelize.STRING },
-  email: { type: Sequelize.STRING },
-  full_name: { type: Sequelize.STRING },
-  phone_number: { type: Sequelize.STRING(10) },
-  tasks_completed: { type: Sequelize.INTEGER },
-  events_attended: { type: Sequelize.INTEGER },
-  location: { type: Sequelize.STRING },
-  avatar_id: { type: Sequelize.INTEGER },
-  avatar_shirt: { type: Sequelize.STRING },
-  avatar_pants: { type: Sequelize.STRING },
+  username: { type: DataTypes.STRING(20) },
+  google_id: { type: DataTypes.STRING },
+  email: { type: DataTypes.STRING },
+  full_name: { type: DataTypes.STRING },
+  phone_number: { type: DataTypes.STRING(10) },
+  tasks_completed: { type: DataTypes.INTEGER },
+  events_attended: { type: DataTypes.INTEGER },
+  location: { type: DataTypes.STRING },
+  avatar_id: { type: DataTypes.INTEGER },
+  avatar_shirt: { type: DataTypes.STRING },
+  avatar_pants: { type: DataTypes.STRING },
 });
 
 export default User;
