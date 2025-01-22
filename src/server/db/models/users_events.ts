8680a59ccbdc041ae75545c29b10e5f857695e1d
tymey <tyler.meyer.a@gmail.com>
2025-01-22 17:26:45 -0600

    update: Remove user_id & event_id from User_Events table

--- src/server/db/models/users_events.ts
+++ src/server/db/models/users_events.ts
@@ -4,15 +4,13 @@ import User from './users';
 import Event from './events';
 
 const User_Event = database.define('User_Event', {
-    user_id: { type: Sequelize.INTEGER },
-    event_id: { type: Sequelize.INTEGER },
-    user_attended: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
-    },
-    private_review: { type: Sequelize.STRING(250) },
-    public_review: { type: Sequelize.STRING(250) },
-})
+  user_attended: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  private_review: { type: Sequelize.STRING(250) },
+  public_review: { type: Sequelize.STRING(250) },
+});
 
 User.belongsToMany(Event, { through: User_Event });
 Event.belongsToMany(User, { through: User_Event });
