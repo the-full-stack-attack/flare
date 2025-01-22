e5e9b5cfc768b454bafe665795dbaaeb4b39c78c
coltongraygg <coltongraygg@gmail.com>
2025-01-22 10:38:19 -0600

    MERGING

--- src/client/views/CreateEvents.tsx
+++ src/client/views/CreateEvents.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useState } from 'react';
 
 function CreateEvents() {
 
+    const [formData, setFormData] = useState();
+
+    const onSubmit = () => {
+
+    }
     return (<>
             <div>
                 <h4>Hello Stanky</h4>
