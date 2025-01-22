69472a496d5d7524e700959b53c6224e07a14392
coltongraygg <coltongraygg@gmail.com>
2025-01-21 19:07:20 -0600

    init form

--- src/client/views/CreateEvents.tsx
+++ src/client/views/CreateEvents.tsx
@@ -1,11 +1,53 @@
 import React from 'react';
 
 function CreateEvents() {
-  return (
-    <div>
-      <h4>Hello Stanky</h4>
-    </div>
-  )
+
+    return (<>
+            <div>
+                <h4>Hello Stanky</h4>
+            </div>
+
+            <div>
+                <form>
+                    <label>Title
+                        <input name='event-title'/>
+                    </label>
+                    <label>Description
+                        <input name='event-description'/>
+                    </label>
+
+                    <label>Start Date
+                        <input name='event-start-date'/>
+                    </label>
+                    <label>End Date
+                        <input name='event-end-date'/>
+                    </label>
+
+                    <label>Start Time
+                        <input name='event-start-time'/>
+                    </label>
+
+                    <label>
+                        <input name='event-end-time'/>
+                    </label>
+
+                    <label>Event Category
+                        <input name='event-category'/>
+                    </label>
+
+                    <label>Event Interests?
+                        <input name='event-interests'/>
+                    </label>
+
+                    <label>Venue
+                        <input name='event-venue'/>
+                    </label>
+
+                    <button type='submit'>Submit</button>
+                </form>
+            </div>
+        </>
+    )
 };
 
 export default CreateEvents;
