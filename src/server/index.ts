27bbb369ab3189aa6c1253d71bcfdf2c7dab0f17
bkhebert <bondman2004tippy@gmail.com>
2025-01-22 19:30:26 -0600

    REVISE ensured the socket version of the app is using port 4000

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,24 +1,40 @@
-import ('./db/index');
-import { Server } from 'socket.io';
 import https from 'https';
+import http from 'http';
 import fs from 'fs';
+import { Server } from 'socket.io';
 import app from './app';
 import database from './db/index';
+
+import('./db/index');
 
 const PORT = 4000;
 
 if (process.env.DEVELOPMENT === 'true') {
   database
-  .sync({ alter: true })
-  .then(() => {
-    app.listen(PORT, '0.0.0.0', () => {
-      console.log(`Listening on http://localhost:${PORT}`);
+    .sync({ alter: true })
+    .then(() => {
+      if (process.env.SOCKET !== 'true') {
+        app.listen(PORT, '0.0.0.0', () => {
+          console.log(`Listening on http://localhost:${PORT}`);
+        });
+      } else {
+        const server = http.createServer(app);
+        const io = new Server(server);
+        // Register event listeners for Socket.IO
+        io.on('connection', (socket) => {
+          console.log('a user connected');
+          // Handle socket events here
+        });
+
+        server.listen(4000, () => {
+          console.log('listening on *:4000');
+        });
+      }
+    })
+    .catch((err: Error) => {
+      console.error(err, 'oops!');
     });
-  })
-  .catch((err: Error) => {
-    console.error(err, 'oops!');
-    });
-  } else {
+} else {
   const options = {
     cert: fs.readFileSync('/etc/letsencrypt/live/slayer.events/fullchain.pem'),
     key: fs.readFileSync('/etc/letsencrypt/live/slayer.events/privkey.pem'),
@@ -26,7 +42,7 @@ if (process.env.DEVELOPMENT === 'true') {
 
   const io = new Server(https.createServer(options, app));
   io.on('connection', (socket) => {
-    console.log('connected', socket);
+    socket.emit('connect', { message: 'a new client connected!' });
   });
 
   https.createServer(options, app).listen(443);
