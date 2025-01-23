6e54d9680f9a7eecfa399fe980233bb4c236b926
evan.loria4 <evan.loria4@gmail.com>
2025-01-23 17:02:08 -0600

    Initialize ChooseTask component

---
+++ src/client/components/tasks/ChooseTask.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+} from '../../../components/ui/card';
+
+function ChooseTask() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Pick a Task Category</CardTitle>
+      </CardHeader>
+    </Card>
+  );
+}
+
+export default ChooseTask;
