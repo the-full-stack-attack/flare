40d20a0cd2fc13a442d422085e634551998ff800
tymey <tyler.meyer.a@gmail.com>
2025-01-23 15:28:44 -0600

    feat: Create PATCH /api/event/attending/:id, toggles attending

--- src/server/routes/event2.ts
+++ src/server/routes/event2.ts
@@ -67,20 +67,48 @@ event2Router.post('/attend/:id', (req: any, res: Response) => {
 /*
   GET /api/event/attend => Retrieve all events the user is attending from the User_Events table
 */
-event2Router.get('/attend', (req: any, res: Response) => {
+event2Router.get('/attend/:isAttending', (req: any, res: Response) => {
+  let isAttending = true;
+
+  if (req.params.isAttending === 'false') {
+    isAttending = false;
+  }
+
   User.findOne({
     where: {
       id: req.user.id,
     },
-    include: Event,
+    include: {
+      model: Event,
+      where: { start_time: { [Op.gt]: new Date(Date.now()) } },
+    },
   })
     .then((data) => {
       res.status(200);
-      res.send(data?.dataValues.Events);
+      res.send(
+        data?.dataValues.Events.filter(
+          (event: any) => event.User_Event.user_attending === isAttending
+        )
+      );
     })
     .catch((err: unknown) => {
       console.error('Failed to GET /api/event/attend', err);
     });
+});
+
+/*
+  PATCH /api/event/attending/:id => Update User_Events row's user_attending to the opposite boolean
+    - req.params => { id } (for the User_Events row)
+*/
+event2Router.patch('/attending/:id', async (req: any, res: Response) => {
+  try {
+    const userEvent: any = await User_Event.findByPk(req.params.id);
+    userEvent.user_attending = !userEvent.user_attending;
+    await userEvent.save();
+    res.sendStatus(200);
+  } catch (err: unknown) {
+    console.error('Failed to PATCH /api/event/attend/:id', err);
+  }
 });
 
 export default event2Router;
