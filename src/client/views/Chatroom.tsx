e3523546014e1e43b01acfe1bcbc2b9687f6da91
bkhebert <bondman2004tippy@gmail.com>
2025-01-22 19:42:22 -0600

    ADD basic layout client-side Chatroom

--- src/client/views/Chatroom.tsx
+++ src/client/views/Chatroom.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:4000');
 
 function Chatroom() {
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit('message', message);
+    setMessage('');
+  };
+
   return (
     <div>
-      <h4>Hello Stanky</h4>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
     </div>
-  )
-};
+  );
+}
 
 export default Chatroom;
