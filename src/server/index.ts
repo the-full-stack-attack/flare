88d8ed5d465ea6fd6a0557927b1d5700a5ade328
bkhebert <bondman2004tippy@gmail.com>
2025-01-22 19:08:45 -0600

    SETUP socket io to connect to https deployed server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import ('./db/index');
+import { Server } from 'socket.io';
 import https from 'https';
 import fs from 'fs';
 import app from './app';
@@ -8,19 +9,25 @@ const PORT = 4000;
 
 if (process.env.DEVELOPMENT === 'true') {
   database
-    .sync({ alter: true })
-    .then(() => {
-      app.listen(PORT, '0.0.0.0', () => {
-        console.log(`Listening on http://localhost:${PORT}`);
-      });
-    })
-    .catch((err: Error) => {
-      console.error(err, 'oops!');
+  .sync({ alter: true })
+  .then(() => {
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Listening on http://localhost:${PORT}`);
     });
-} else {
+  })
+  .catch((err: Error) => {
+    console.error(err, 'oops!');
+    });
+  } else {
   const options = {
     cert: fs.readFileSync('/etc/letsencrypt/live/slayer.events/fullchain.pem'),
     key: fs.readFileSync('/etc/letsencrypt/live/slayer.events/privkey.pem'),
   };
+
+  const io = new Server(https.createServer(options, app));
+  io.on('connection', (socket) => {
+    console.log('connected', socket);
+  });
+
   https.createServer(options, app).listen(443);
 }
