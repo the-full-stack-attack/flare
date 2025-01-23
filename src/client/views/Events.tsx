791220b061641c2dba5661687a4c7e4d33c9a649
tymey <tyler.meyer.a@gmail.com>
2025-01-22 20:01:43 -0600

    feat: Fetch all user's attending events on mount

--- src/client/views/Events.tsx
+++ src/client/views/Events.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import EventsList from '../components/events-view/EventsList';
+import AttendingEventsList from '../components/events-view/AttendingEventsList';
 
 type GeoPosition = {
   coords: {
@@ -29,6 +30,23 @@ type EventData = {
   updatedAt: Date;
 };
 
+type AttendingEventData = {
+  id: number;
+  title: string;
+  start_time: Date;
+  end_time: Date;
+  address: string;
+  description: string;
+  venue_id: number;
+  created_by: number;
+  chatroom_id: number;
+  createdAt: Date;
+  updatedAt: Date;
+  User_Event: {
+    user_attending: boolean;
+  };
+};
+
 function Events() {
   // The latitude and longitude of the user will be stored in state on page load
   const [location, setLocation] = useState<Location>({
@@ -38,6 +56,11 @@ function Events() {
 
   // Events the user can attend will be stored in state on page load
   const [events, setEvents] = useState<EventData[]>([]);
+
+  // Events the user can attend will be stored in state on page load
+  const [attendingEvents, setAttendingEvents] = useState<AttendingEventData[]>(
+    []
+  );
 
   const getGeoLocation = () => {
     const success = (position: GeoPosition) => {
@@ -68,16 +91,31 @@ function Events() {
       });
   };
 
+  const getAttendEvents = () => {
+    axios
+      .get('/api/event/attend')
+      .then(({ data }) => {
+        setAttendingEvents(data);
+      })
+      .catch((err: unknown) => {
+        console.error('Failed to getAttendEvents:', err);
+      });
+  };
+
   useEffect(() => {
     getGeoLocation();
     getEvents();
+    getAttendEvents();
   }, []);
+
+  console.log(attendingEvents);
 
   return (
     <div className="container mx-auto px-4 content-center">
       <h1 className="text-2xl font-bold text-center">Events In Your Area</h1>
       <EventsList events={events} />
       <h1 className="text-2xl font-bold text-center">Events Attending</h1>
+      <AttendingEventsList events={attendingEvents} />
     </div>
   );
 }
