9d42f1c12b623c14e89b6e82e036e8a817853f4a
evan.loria4 <evan.loria4@gmail.com>
2025-01-23 14:09:00 -0600

    Render task display to the dashboard

--- src/client/views/Dashboard.tsx
+++ src/client/views/Dashboard.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import TaskDisplay from '../components/tasks/TaskDisplay';
 
 function Dashboard() {
   return (
     <div>
+      <TaskDisplay />
       <h4>Hello Stanky, you have reached the dashboard.</h4>
     </div>
   );
