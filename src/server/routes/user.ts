b88a7fa6a309909e8d4d4be65bba15e17fe53f08
tymey <tyler.meyer.a@gmail.com>
2025-01-24 17:13:34 -0600

    feat: Add GET /api/user handler, sends back user info

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -1,4 +1,21 @@
 import { Router } from 'express';
+import User from '../db/models/users';
+
 const userRouter = Router();
+
+userRouter.get('/', (req: any, res: any) => {
+  if (!req.user) {
+    res.status(200).send({ id: 0 });
+  } else {
+    User.findByPk(req.user.id)
+      .then((userData) => {
+        res.status(200).send(userData);
+      })
+      .catch((err: unknown) => {
+        console.error('Failed to GET /api/user', err);
+        res.sendStatus(500);
+      });
+  }
+});
 
 export default userRouter;
