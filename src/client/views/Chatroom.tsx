60f2badd94d7b53ac9b4d278f0176fbb0204793d
bkhebert <bondman2004tippy@gmail.com>
2025-01-23 21:21:26 -0600

    ADD pixi stage and sprite to chatroom

--- src/client/views/Chatroom.tsx
+++ src/client/views/Chatroom.tsx
@@ -3,18 +3,43 @@ import io from 'socket.io-client';
 import { Input } from '../../components/ui/input';
 import { Label } from '../../components/ui/label';
 import { Button } from '../../components/ui/button';
-import { Application, extend, Stage } from '@pixi/react'
+import { Application, extend, useAssets } from '@pixi/react';
+import { Container, Graphics, Sprite, Texture, Assets } from 'pixi.js';
 
-import { Container, Graphics } from 'pixi.js'
-
+// 'extend' is unique to the beta version of pixi.js
+// With this beta version, everything you import from pixijs
+// must be passed into extend. Then you can utilize them as components
+// prefixed with pixi ex. <pixiContainer/> <pixiGraphics/>
 extend({
   Container,
   Graphics,
-})
+  Sprite,
+  Texture,
+});
 
 const socket = io('http://localhost:4000');
 
 function Chatroom() {
+  // useAssets is how images are loaded into Application
+  useAssets([
+    'https://pixijs.com/assets/bunny.png',
+    {
+      alias: 'bunny',
+      src: 'https://pixijs.com/assets/bunny.png',
+    },
+  ]);
+
+  // useCallback works for drawing circle
+  const drawCallback = useCallback((graphics: unknown) => {
+    // graphics.clear()
+    graphics?.setFillStyle({ color: 'red' });
+    graphics?.circle(100, 100, 50);
+    graphics?.fill();
+  }, []);
+
+  const [playerY, setPlayerY] = useState(0);
+  const [playerX, setPlayerX] = useState(0);
+  const [playerPosition, setPlayerPosition] = useState([playerY, playerX]);
 
   const [message, setMessage] = useState('');
   // const app = useApp();
@@ -31,6 +56,14 @@ function Chatroom() {
     });
   }, []);
 
+  useEffect(() => {
+    socket.on('newPosition', (data) => {
+      setPlayerX(data.x);
+      setPlayerY(data.y);
+      // Update UI with the new message
+    });
+  }, []);
+
   const sendMessage = () => {
     socket.emit('message', message);
     setMessage('');
@@ -39,7 +72,24 @@ function Chatroom() {
   return (
     <div>
       <div>
-        <Application></Application>
+        <Application>
+          <pixiContainer x={100} y={200}>
+            <pixiGraphics draw={drawCallback} />
+          </pixiContainer>
+          <pixiContainer x={200} y={100}>
+            <pixiSprite
+              texture={Assets.get('https://pixijs.com/assets/bunny.png')}
+              x={playerX}
+              y={playerY}
+              width={20}
+              height={20}
+            />
+
+            {/* <pixiSprite
+       
+        ></pixiSprite> */}
+          </pixiContainer>
+        </Application>
       </div>
       <Label> Oi, put a message in stinky!</Label>
       <Input
