f9243fbf9d90fa1eea918e8060b0b24fd99ca5c1
evan.loria4 <evan.loria4@gmail.com>
2025-01-23 21:19:03 -0600

    Create types and assign state values

--- src/client/views/Dashboard.tsx
+++ src/client/views/Dashboard.tsx
@@ -3,20 +3,28 @@ import axios from 'axios';
 import TaskDisplay from '../components/tasks/TaskDisplay';
 import ChooseTask from '../components/tasks/ChooseTask';
 
+type User = {
+  id: number;
+  current_task_id: number;
+};
+type SetUser = (user: User) => User;
+
 function Dashboard() {
-  const [user, setUser] = useState({});
+  const [user, setUser]: [User, Function] = useState({
+    id: 0,
+    current_task_id: 0,
+  });
 
   useEffect(() => {
     axios
       .get('/api/user')
       .then(({ data }) => {
-        console.log(data);
         setUser(data);
       })
       .catch((err: Error) => {
         console.error('Error GETting the user in the dashboard: ', err);
       });
-  });
+  }, []);
   return (
     <>
       <h4>Hello Stanky, you have reached the dashboard.</h4>
