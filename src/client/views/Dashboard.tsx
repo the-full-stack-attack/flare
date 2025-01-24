7b038c2816bbf6826babc6e99baf368fd30714f2
evan.loria4 <evan.loria4@gmail.com>
2025-01-23 20:21:12 -0600

    Make a function to get the user on comp mounting

--- src/client/views/Dashboard.tsx
+++ src/client/views/Dashboard.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import TaskDisplay from '../components/tasks/TaskDisplay';
+import ChooseTask from '../components/tasks/ChooseTask';
 
 function Dashboard() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    axios
+      .get('/api/user')
+      .then(({ data }) => {
+        console.log(data);
+        setUser(data);
+      })
+      .catch((err: Error) => {
+        console.error('Error GETting the user in the dashboard: ', err);
+      });
+  });
   return (
-    <div>
-      <TaskDisplay />
+    <>
       <h4>Hello Stanky, you have reached the dashboard.</h4>
-    </div>
+      <TaskDisplay user={user} />
+      <ChooseTask user={user} />
+    </>
   );
 }
 
