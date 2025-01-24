eafa2171a6f7aac1957f9fabc34c4ddc7c63db32
evan.loria4 <evan.loria4@gmail.com>
2025-01-23 19:58:09 -0600

    Handle GET req to /api/user

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -1,4 +1,23 @@
 import { Router } from 'express';
+import User from '../db/models/users';
+
 const userRouter = Router();
+
+// GET request to /api/user
+userRouter.get('/', (req, res) => {
+  const { id } = req.user;
+  console.log(req.user);
+  User.findByPk(id)
+    .then((user) => {
+      if (!user) {
+        res.sendStatus(400);
+      } else {
+        res.status(200).send(user);
+      }
+    })
+    .catch((err) => {
+      console.error('Error finding user: ', err);
+    });
+});
 
 export default userRouter;
