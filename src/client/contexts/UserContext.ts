2068b6400abc5427a44931a7fb9369aa2e1085f4
tymey <tyler.meyer.a@gmail.com>
2025-01-24 17:13:02 -0600

    feat: Create UserContext for application

---
+++ src/client/contexts/UserContext.ts
@@ -0,0 +1,26 @@
+import { createContext } from 'react';
+
+export interface UserType {
+  id: number;
+  username?: string;
+  google_id?: string;
+  email?: string;
+  full_name?: string;
+  phone_number?: string;
+  tasks_complete?: number;
+  events_attended?: number;
+  location?: string;
+  avatar_id?: number;
+  avatar_shirt?: string;
+  avatar_pants?: string;
+}
+
+type UserContextType = {
+  user: UserType;
+  setUser: (user: UserType) => void;
+};
+
+export const UserContext = createContext<UserContextType>({
+  user: { id: 0 },
+  setUser: () => {},
+});
