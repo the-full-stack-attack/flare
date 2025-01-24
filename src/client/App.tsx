0afc7437432e559f97cfe8cd330e19ada4a8cfc7
tymey <tyler.meyer.a@gmail.com>
2025-01-24 16:08:57 -0600

    feat: Create User Context for the entire app.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, createContext, useMemo } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import {
   AiConversations,
@@ -13,20 +13,56 @@ import {
 import NavBar from './components/NavBar';
 import './styles/main.css';
 
+interface User {
+  id: number;
+  username?: string;
+  google_id?: string;
+  email?: string;
+  full_name?: string;
+  phone_number?: string;
+  tasks_complete?: number;
+  events_attended?: number;
+  location?: string;
+  avatar_id?: number;
+  avatar_shirt?: string;
+  avatar_pants?: string;
+}
+
+type UserContextType = {
+  user: User;
+  setUser: (user: User) => void;
+};
+
+const UserContext = createContext<UserContextType>({
+  user: { id: 0 },
+  setUser: () => {},
+});
+
 export default function App() {
+  const [user, setUser] = useState<User>({ id: 0 });
+  const userState = useMemo(
+    () => ({
+      user,
+      setUser,
+    }),
+    [user]
+  );
+
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="AiConversations" element={<AiConversations />} />
-        <Route path="Chatroom" element={<Chatroom />} />
-        <Route path="CreateEvents" element={<CreateEvents />} />
-        <Route path="Events" element={<Events />} />
-        <Route path="Signup" element={<Signup />} />
-        <Route path="Task" element={<Task />} />
-        <Route path="Dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <UserContext.Provider value={userState}>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="AiConversations" element={<AiConversations />} />
+          <Route path="Chatroom" element={<Chatroom />} />
+          <Route path="CreateEvents" element={<CreateEvents />} />
+          <Route path="Events" element={<Events />} />
+          <Route path="Signup" element={<Signup />} />
+          <Route path="Task" element={<Task />} />
+          <Route path="Dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
