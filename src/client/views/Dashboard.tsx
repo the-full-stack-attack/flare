800a98dab2ee2db283afb1b5a95468367030eefc
evan.loria4 <evan.loria4@gmail.com>
2025-01-24 18:57:51 -0600

    Create getTask function and use it with useEffect

--- src/client/views/Dashboard.tsx
+++ src/client/views/Dashboard.tsx
@@ -7,6 +7,20 @@ import { UserContext } from '../contexts/UserContext';
 function Dashboard() {
   const { user, setUser } = useContext(UserContext);
   const [task, setTask] = useState();
+  // Function to set the task in state
+  const getTask = (): void => {
+    const { current_task_id } = user;
+    axios
+      .get(`/api/task/${current_task_id}`)
+      .then(({ data }) => {
+        setTask(data);
+      })
+      .catch((err) => {
+        console.error('Error GETting task by id: ', err);
+      });
+  };
+  // Use effect will call getTask if there is a change in user state
+  useEffect(getTask, [user]);
   return (
     <>
       <h4>Hello Stanky, you have reached the dashboard.</h4>
