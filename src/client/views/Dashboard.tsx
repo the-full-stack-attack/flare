223c7890a2ed78fce1074ff34949dc8dfc0dbfa3
evan.loria4 <evan.loria4@gmail.com>
2025-01-24 18:48:45 -0600

    Remove getUser function from Dashboard

--- src/client/views/Dashboard.tsx
+++ src/client/views/Dashboard.tsx
@@ -1,33 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import TaskDisplay from '../components/tasks/TaskDisplay';
 import ChooseTask from '../components/tasks/ChooseTask';
-
-type User = {
-  id: number;
-  current_task_id: number;
-};
-type SetUser = (user: User) => User;
+import { UserContext } from '../contexts/UserContext';
 
 function Dashboard() {
-  const [user, setUser]: [User, Function] = useState({
-    id: 0,
-    current_task_id: 0,
-  });
-  const [task, setTask] = useState({});
-
-  const getUser = () => {
-    axios
-      .get('/api/user')
-      .then(({ data }) => {
-        console.log('Data from user: ', data);
-        setUser(data);
-      })
-      .catch((err: Error) => {
-        console.error('Error GETting the user in the dashboard: ', err);
-      });
-  };
-  useEffect(getUser, []);
+  const { user, setUser } = useContext(UserContext);
+  const [task, setTask] = useState();
   return (
     <>
       <h4>Hello Stanky, you have reached the dashboard.</h4>
