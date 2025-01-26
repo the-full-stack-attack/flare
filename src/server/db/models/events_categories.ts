f342000f36e3bddbef834a453ff56b474235c768
evan.loria4 <evan.loria4@gmail.com>
2025-01-25 23:20:14 -0600

    Fix server error multiple primary keys in event_categories

--- src/server/db/models/events_categories.ts
+++ src/server/db/models/events_categories.ts
@@ -1,11 +1,11 @@
 import Sequelize from 'sequelize';
 import database from '../index';
+import Event from './events';
+import Category from './categories';
 
+const Event_Category = database.define('Event_Category', {});
 
-const Event_Category = database.define('Event_Category', {
-    event_id: { type: Sequelize.INTEGER, },
-    category_id: { type: Sequelize.INTEGER, },
-});
-
-
+// Declare the foreign key for the user id and the task id
+Event.belongsToMany(Category, { through: Event_Category });
+Category.belongsToMany(Event, { through: Event_Category });
 export default Event_Category;
