9ca95d64a9aa2cccd1171743f76a3133362b865d
tymey <tyler.meyer.a@gmail.com>
2025-01-25 20:14:45 -0600

    feat: Set location state to info from Google Geocoding

--- src/client/views/Events.tsx
+++ src/client/views/Events.tsx
@@ -19,7 +19,7 @@ type GeoPosition = {
   };
 };
 
-type Location = {
+type GeoLocation = {
   latitude: null | number;
   longitude: null | number;
 };
@@ -45,10 +45,12 @@ function Events() {
   const { user } = useContext(UserContext);
 
   // The latitude and longitude of the user will be stored in state on page load
-  const [location, setLocation] = useState<Location>({
+  const [geoLocation, setGeoLocation] = useState<GeoLocation>({
     latitude: 0,
     longitude: 0,
   });
+
+  const [location, setLocation] = useState<any>({});
 
   // Events the user can attend will be stored in state on page load
   const [events, setEvents] = useState<EventData[]>([]);
@@ -62,7 +64,7 @@ function Events() {
     const success = (position: GeoPosition) => {
       const { latitude, longitude } = position.coords;
 
-      setLocation({ latitude, longitude });
+      setGeoLocation({ latitude, longitude });
     };
 
     const error = () => {
@@ -116,11 +118,13 @@ function Events() {
   };
 
   const getLocation = () => {
-    if (location.latitude && location.longitude) {
+    if (geoLocation.latitude && geoLocation.longitude) {
       axios
-        .get(`/api/event/location/${location.latitude}/${location.longitude}`)
+        .get(
+          `/api/event/location/${geoLocation.latitude}/${geoLocation.longitude}`
+        )
         .then(({ data }) => {
-          console.log(data);
+          setLocation(data);
         })
         .catch((err: unknown) => {
           console.error('Failed getLocation:', err);
@@ -135,12 +139,13 @@ function Events() {
 
   useEffect(() => {
     getLocation();
-  }, [location]);
+  }, [geoLocation]);
 
   // console.log('Events:', events);
   // console.log('Attending Events:', attendingEvents);
   // console.log('Bailed Events:', bailedEvents);
-  console.log(location);
+  console.log('GeoLocation:', geoLocation);
+  console.log('Location:', location);
 
   return (
     <div className="container mx-auto px-4 content-center">
