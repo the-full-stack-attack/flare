1af547e45fc610eb7854f219755862be49c31fa5
evan.loria4 <evan.loria4@gmail.com>
2025-01-28 13:38:46 -0600

    Map over completedTasks in state

--- src/client/components/tasks/CompletedTaskList.tsx
+++ src/client/components/tasks/CompletedTaskList.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import { UserContext } from '../../contexts/UserContext';
+import CompletedTask from './CompletedTask';
 
-function CompletedTaskList() => {
+type CompletedTasks = UserTask[];
+type UserTask = {
+  completed?: boolean;
+  overall_rating: number;
+  date_completed: dayjs.Dayjs;
+  opted_out?: boolean;
+  UserId: number;
+  TaskId: number;
+};
+
+function CompletedTaskList() {
+  const { user } = useContext(UserContext);
+  const [completedTasks, setCompletedTasks] = useState<CompletedTasks>([]);
+
+  useEffect(() => {
+    const { id } = user;
+    axios
+      .get('/api/user_task/:id')
+      .then(({ data }) => {
+        setCompletedTasks(data);
+      })
+      .catch((err) => {
+        console.error('Error fetching user_tasks: ', err);
+      });
+  });
   return (
-    <>
-    </>
-  );
+  <ul>
+    {completedTasks.map((task) => <CompletedTask key={task.createdAt} task={task}/>)}
+  </ul>;
+  )
 }
 
 export default CompletedTaskList;
