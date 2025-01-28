4722c83f54c4b02aface50115aafdec340603a9f
tymey <tyler.meyer.a@gmail.com>
2025-01-28 13:54:25 -0600

    build: seedEvents now adds notifications for events

--- src/server/db/seeds/seedEvents.ts
+++ src/server/db/seeds/seedEvents.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import database from '../index';
 import Event from '../models/events';
 import Venue from '../models/venues';
@@ -5,6 +6,7 @@ import User from '../models/users';
 import Category from '../models/categories';
 import Interest from '../models/interests';
 import Event_Interest from '../models/events_interests';
+import Notification from '../models/notifications';
 
 // EventData type to store events in DB
 type EventData = {
@@ -16,6 +18,7 @@ type EventData = {
   venue_id?: number;
   category_id?: number;
   created_by?: number;
+  hour_before_notif?: number;
   chatroom_id: number;
 };
 
@@ -66,24 +69,24 @@ const adminUser: UserData = {
 const sampleEvents: EventData[] = [
   {
     title: 'Darts Night',
-    start_time: new Date(2025, 0, 31, 18),
-    end_time: new Date(2025, 0, 31, 21),
+    start_time: new Date(now + 1000 * 60 * 60 * 3),
+    end_time: new Date(now + 1000 * 60 * 60 * 4),
     address: '123 Have Fun Blvd',
     description: "We're playing darts.",
     chatroom_id: 1,
   },
   {
     title: 'Movie Night',
-    start_time: new Date(2025, 0, 31, 19),
-    end_time: new Date(2025, 0, 31, 22),
+    start_time: new Date(now + 1000 * 60 * 60 * 5),
+    end_time: new Date(now + 1000 * 60 * 60 * 6),
     address: '456 Laughing St',
     description: "We're watching The Jerk.",
     chatroom_id: 1,
   },
   {
     title: 'Watching the Game',
-    start_time: new Date(2025, 1, 1, 18, 30),
-    end_time: new Date(2025, 1, 1, 23),
+    start_time: new Date(now + 1000 * 60 * 60 * 7),
+    end_time: new Date(now + 1000 * 60 * 60 * 8),
     address: '789 Game Ave',
     description: "We're watching The Saints game.",
     chatroom_id: 1,
@@ -123,49 +126,85 @@ const seedEvents = async () => {
       event.category_id = categories[Math.floor(Math.random() * categories.length)].id;
     });
 
+    const addNotifications = async (events: any) => {
+      for (const event of events) {
+        const hourBeforeNotif: any = await Notification.create({
+          message: `The upcoming event you're attending, ${event.title}, starts soon at ${dayjs(event.start_time).format('h:mm A')}. Hope to see you there.`,
+          send_time: new Date(event.start_time.getTime() - 1000 * 60 * 60),
+        });
+        event.hour_before_notif = hourBeforeNotif.dataValues.id;
+      }
+    };
+
+    await addNotifications(sampleEvents);
+
+    console.log(sampleEvents);
+
     // Create the events for the database
     const newEvents: any = await Event.bulkCreate(sampleEvents);
 
-    newEvents.forEach(async (event: any) => {
-      const EventId = event.dataValues.id;
+    const addInterests = async (events: any) => {
+      for (let event of events) {
+        const EventId = event.dataValues.id;
+        for (let i = 0; i < 1 + Math.floor(Math.random() * 3); i += 1) {
+          await Event_Interest.findOrCreate({
+            where: {
+              EventId,
+              InterestId:
+                interests[Math.floor(Math.random() * interests.length)].id,
+            },
+            defaults: {
+              EventId,
+              InterestId:
+                interests[Math.floor(Math.random() * interests.length)].id,
+            },
+          });
+        }
+      }
+    };
 
-      await Event_Interest.findOrCreate({
-        where: {
-          EventId,
-          InterestId:
-            interests[Math.floor(Math.random() * interests.length)].id,
-        },
-        defaults: {
-          EventId,
-          InterestId:
-            interests[Math.floor(Math.random() * interests.length)].id,
-        },
-      });
-      await Event_Interest.findOrCreate({
-        where: {
-          EventId,
-          InterestId:
-            interests[Math.floor(Math.random() * interests.length)].id,
-        },
-        defaults: {
-          EventId,
-          InterestId:
-            interests[Math.floor(Math.random() * interests.length)].id,
-        },
-      });
-      await Event_Interest.findOrCreate({
-        where: {
-          EventId,
-          InterestId:
-            interests[Math.floor(Math.random() * interests.length)].id,
-        },
-        defaults: {
-          EventId,
-          InterestId:
-            interests[Math.floor(Math.random() * interests.length)].id,
-        },
-      });
-    });
+    await addInterests(newEvents);
+
+    // newEvents.forEach(async (event: any) => {
+    //   const EventId = event.dataValues.id;
+
+    //   await Event_Interest.findOrCreate({
+    //     where: {
+    //       EventId,
+    //       InterestId:
+    //         interests[Math.floor(Math.random() * interests.length)].id,
+    //     },
+    //     defaults: {
+    //       EventId,
+    //       InterestId:
+    //         interests[Math.floor(Math.random() * interests.length)].id,
+    //     },
+    //   });
+    //   await Event_Interest.findOrCreate({
+    //     where: {
+    //       EventId,
+    //       InterestId:
+    //         interests[Math.floor(Math.random() * interests.length)].id,
+    //     },
+    //     defaults: {
+    //       EventId,
+    //       InterestId:
+    //         interests[Math.floor(Math.random() * interests.length)].id,
+    //     },
+    //   });
+    //   await Event_Interest.findOrCreate({
+    //     where: {
+    //       EventId,
+    //       InterestId:
+    //         interests[Math.floor(Math.random() * interests.length)].id,
+    //     },
+    //     defaults: {
+    //       EventId,
+    //       InterestId:
+    //         interests[Math.floor(Math.random() * interests.length)].id,
+    //     },
+    //   });
+    // });
 
     console.log('Created events');
   } catch (err: unknown) {
