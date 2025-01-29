8989d6473ead5aa7ca9c010a3d9c050c9a3fe72d
evan.loria4 <evan.loria4@gmail.com>
2025-01-29 16:53:13 -0600

    Change dialog for choose task to use DialogBox component

--- src/client/components/tasks/ChooseTask.tsx
+++ src/client/components/tasks/ChooseTask.tsx
@@ -11,6 +11,7 @@ import {
 } from '../../../components/ui/card';
 import TypeButton from './TypeButton';
 import DifficultyButton from './DifficultyButton';
+import DialogBox from './DialogBox';
 import { UserContext } from '../../contexts/UserContext';
 
 type TaskInfo = {
@@ -54,41 +55,15 @@ function ChooseTask() {
   };
   return (
     <>
-      <Dialog
-        open={isOpen}
-        as="div"
-        className="relative z-10 focus:outline-none"
-        onClose={() => setIsOpen(false)}
-      >
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4">
-            <DialogPanel
-              transition
-              className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
-            >
-              <DialogTitle
-                as="h3"
-                className="text-base/7 font-medium text-white"
-              >
-                ChooseTask
-              </DialogTitle>
-              <p className="mt-2 text-sm/6 text-white/50">
-                {`Do you want to choose the Level ${taskInfo.difficulty} ${taskInfo.type} task?`}
-              </p>
-              <div className="mt-4">
-                <Button
-                  onClick={() => {
-                    setIsOpen(false);
-                  }}
-                >
-                  NO
-                </Button>
-                <Button onClick={chooseTask}>YES</Button>
-              </div>
-            </DialogPanel>
-          </div>
-        </div>
-      </Dialog>
+      <DialogBox
+        isOpen={isOpen}
+        confirm={chooseTask}
+        stateSetter={setIsOpen}
+        title="Confirm Task"
+        content={`Do you want to choose the Level ${taskInfo.difficulty} ${taskInfo.type} task?`}
+        cancelText="Cancel"
+        confirmText="Confirm"
+      />
       <Card>
         <CardHeader>
           <CardTitle>Choose A Task</CardTitle>
