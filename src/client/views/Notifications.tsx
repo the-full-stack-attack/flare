d5868685ce18f0329d8f0a04aa3d39ad6ef10962
tymey <tyler.meyer.a@gmail.com>
2025-01-28 20:02:53 -0600

    feat: User can see their notifications, need to clean up

--- src/client/views/Notifications.tsx
+++ src/client/views/Notifications.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { UserContext } from '../contexts/UserContext';
 
 function Notifications() {
+  const { user } = useContext(UserContext);
+  console.log(user);
   return (
     <div>
-      <h1>Notifications</h1>
+      <h1 className="text-4xl">Notifications</h1>
+      <ul>
+        {user.Notifications.map((notif: any) => (
+          <li>{notif.message}</li>
+        ))}
+      </ul>
     </div>
   );
 }
