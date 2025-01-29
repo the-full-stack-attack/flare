42714ff4a98a548efdac9927418f7fda4270ffe6
evan.loria4 <evan.loria4@gmail.com>
2025-01-29 15:56:01 -0600

    Add dialog box to ChooseTask

--- src/client/components/tasks/ChooseTask.tsx
+++ src/client/components/tasks/ChooseTask.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
 import dayjs from 'dayjs';
 import axios from 'axios';
+import { Button, Dialog, DialogPanel, DialogTitle } from '@headlessui/react';
 import {
   Card,
   CardHeader,
@@ -8,7 +9,6 @@ import {
   CardContent,
   CardFooter,
 } from '../../../components/ui/card';
-import { Button } from '../../../components/ui/button';
 import TypeButton from './TypeButton';
 import DifficultyButton from './DifficultyButton';
 import { UserContext } from '../../contexts/UserContext';
@@ -22,6 +22,7 @@ type TaskInfo = {
 const types: string[] = ['Fun', 'Active', 'Normal', 'Duo', 'Rejection Therapy'];
 const difficulties: number[] = [1, 2, 3, 4, 5];
 function ChooseTask() {
+  const [isOpen, setIsOpen] = useState(false);
   const [taskInfo, setTaskInfo] = useState<TaskInfo>({
     type: '',
     difficulty: 3,
@@ -44,41 +45,86 @@ function ChooseTask() {
       .then(() => {
         getUser();
       })
+      .then(() => {
+        setIsOpen(false);
+      })
       .catch((err) => {
         console.error('Error posting task: ', err);
       });
   };
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Choose A Task</CardTitle>
-      </CardHeader>
-      <CardContent>
-        Choose a Difficulty
-        {difficulties.map((difficulty) => (
-          <DifficultyButton
-            key={difficulty}
-            difficulty={difficulty}
-            taskInfo={taskInfo}
-            setTaskInfo={setTaskInfo}
-          />
-        ))}
-        <br />
-        {types.map((type) => (
-          <TypeButton
-            key={type}
-            type={type}
-            setTaskInfo={setTaskInfo}
-            taskInfo={taskInfo}
-          />
-        ))}
-      </CardContent>
-      <CardFooter>
-        <Button onClick={chooseTask} variant="secondary">
-          Choose Task
-        </Button>
-      </CardFooter>
-    </Card>
+    <>
+      <Dialog
+        open={isOpen}
+        as="div"
+        className="relative z-10 focus:outline-none"
+        onClose={() => setIsOpen(false)}
+      >
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4">
+            <DialogPanel
+              transition
+              className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+            >
+              <DialogTitle
+                as="h3"
+                className="text-base/7 font-medium text-white"
+              >
+                ChooseTask
+              </DialogTitle>
+              <p className="mt-2 text-sm/6 text-white/50">
+                {`Do you want to choose the Level ${taskInfo.difficulty} ${taskInfo.type} task?`}
+              </p>
+              <div className="mt-4">
+                <Button
+                  onClick={() => {
+                    setIsOpen(false);
+                  }}
+                >
+                  NO
+                </Button>
+                <Button onClick={chooseTask}>YES</Button>
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
+      <Card>
+        <CardHeader>
+          <CardTitle>Choose A Task</CardTitle>
+        </CardHeader>
+        <CardContent>
+          Choose a Difficulty
+          {difficulties.map((difficulty) => (
+            <DifficultyButton
+              key={difficulty}
+              difficulty={difficulty}
+              taskInfo={taskInfo}
+              setTaskInfo={setTaskInfo}
+            />
+          ))}
+          <br />
+          {types.map((type) => (
+            <TypeButton
+              key={type}
+              type={type}
+              setTaskInfo={setTaskInfo}
+              taskInfo={taskInfo}
+            />
+          ))}
+        </CardContent>
+        <CardFooter>
+          <Button
+            onClick={() => {
+              setIsOpen(true);
+            }}
+            variant="secondary"
+          >
+            Choose Task
+          </Button>
+        </CardFooter>
+      </Card>
+    </>
   );
 }
 
