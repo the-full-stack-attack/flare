c6520ab7dbc8e9da2d5a5bdd8135da968c0cd7a4
evan.loria4 <evan.loria4@gmail.com>
2025-01-29 16:43:47 -0600

    Convert OptOutTask dialog to use DialogBox component

--- src/client/components/tasks/OptOutTask.tsx
+++ src/client/components/tasks/OptOutTask.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import dayjs from 'dayjs';
 import { Button, Dialog, DialogPanel, DialogTitle } from '@headlessui/react';
 import { UserContext } from '../../contexts/UserContext';
+import DialogBox from './DialogBox';
 
 type CompletedTaskProps = {
   userTask: UserTask;
@@ -50,41 +51,15 @@ function OptOutTask({ userTask }: CompletedTaskProps) {
   };
   return (
     <>
-      <Dialog
-        open={isOpen}
-        as="div"
-        className="relative z-10 focus:outline-none"
-        onClose={() => setIsOpen(false)}
-      >
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4">
-            <DialogPanel
-              transition
-              className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
-            >
-              <DialogTitle
-                as="h3"
-                className="text-base/7 font-medium text-white"
-              >
-                Retry Task
-              </DialogTitle>
-              <p className="mt-2 text-sm/6 text-white/50">
-                {`Do you want retry the task ${Task.description.slice(0, -1)}? This will opt you out of your current task if you already have one.`}
-              </p>
-              <div className="mt-4">
-                <Button
-                  onClick={() => {
-                    setIsOpen(false);
-                  }}
-                >
-                  Cancel
-                </Button>
-                <Button onClick={retryTask}>Retry</Button>
-              </div>
-            </DialogPanel>
-          </div>
-        </div>
-      </Dialog>
+      <DialogBox
+        isOpen={isOpen}
+        confirm={retryTask}
+        stateSetter={setIsOpen}
+        title="Retry Task"
+        content={`Do you want retry the task ${Task.description.slice(0, -1)}? This will opt you out of your current task if you already have one.`}
+        cancelText="Cancel"
+        confirmText="Retry"
+      />
       <li>
         {`Opted out Level ${difficulty} ${type} ${description}`}
         <Button
