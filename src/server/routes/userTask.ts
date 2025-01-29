f8ecfc7a46f9953d99af5076ddde423909ddef52
evan.loria4 <evan.loria4@gmail.com>
2025-01-28 22:30:55 -0600

    Handle GET rews to /api/user_task/optedOut/:id

--- src/server/routes/userTask.ts
+++ src/server/routes/userTask.ts
@@ -6,12 +6,33 @@ import User from '../db/models/users';
 
 const userTaskRouter = Router();
 
-// GET requests to /api/userTask/:id
+// GET requests to /api/userTask/complete/:id
 userTaskRouter.get('/complete/:id', async (req: any, res: Response) => {
   // Grab the user's id from the req.params
   const { id } = req.params;
   User_Task.findAll({
     where: { UserId: id, completed: true },
+    include: [Task],
+  })
+    .then((userTasks) => {
+      if (!userTasks) {
+        res.sendStatus(404);
+      } else {
+        res.status(200).send(userTasks);
+      }
+    })
+    .catch((err: any) => {
+      console.error('Error finding the userTasks: ', err);
+      res.sendStatus(500);
+    });
+});
+
+// GET requests to /api/userTask/optOut/:id
+userTaskRouter.get('/optOut/:id', async (req: any, res: Response) => {
+  // Grab the user's id from the req.params
+  const { id } = req.params;
+  User_Task.findAll({
+    where: { UserId: id, opted_out: true },
     include: [Task],
   })
     .then((userTasks) => {
