42b51b3109c311032a8c240048487a9dd6e12581
evan.loria4 <evan.loria4@gmail.com>
2025-01-28 18:34:53 -0600

    Create state in CompletedTask component

--- src/client/components/tasks/CompletedTask.tsx
+++ src/client/components/tasks/CompletedTask.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+import dayjs from 'dayjs';
 
-function CompletedTask() => {
-  return (
-    <>
-    </>
-  );
+type CompletedTaskProps = {
+  task: UserTask;
+};
+type UserTask = {
+  completed?: boolean;
+  overall_rating: number;
+  date_completed: dayjs.Dayjs;
+  opted_out?: boolean;
+  UserId: number;
+  TaskId: number;
+};
+type Task = {
+  id: number;
+  description: string;
+  type: string;
+  completed_count: number;
+  date: dayjs.Dayjs | '';
+  difficulty: number;
+};
+function CompletedTask({ task }: CompletedTaskProps) {
+  const [currTask, setCurrTask] = useState<Task>({
+    id: 0,
+    description: '',
+    type: '',
+    completed_count: 0,
+    date: '',
+    difficulty: 0,
+  });
+
+  useEffect(() => {
+    const { taskId } = task;
+  }, [task]);
+  return <li />;
 }
 
 export default CompletedTask;
