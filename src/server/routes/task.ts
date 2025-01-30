3afed1af279982f099612c7dac0e8187bc81a11f
evan.loria4 <evan.loria4@gmail.com>
2025-01-30 11:41:58 -0600

    Change tasks_completed to total_tasks_completed where necessary

--- src/server/routes/task.ts
+++ src/server/routes/task.ts
@@ -114,7 +114,7 @@ taskRouter.patch('/complete', async (req: any, res: Response) => {
     });
     if (user && task && userTask) {
       user.current_task_id = null;
-      user.tasks_completed += 1;
+      user.total_tasks_completed += 1;
       await user.save();
       task.completed_count += 1;
       await task.save();
