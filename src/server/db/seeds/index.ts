5334ce61030a8c86649ad788a5fe8b25932e97e1
evan.loria4 <evan.loria4@gmail.com>
2025-02-01 16:14:55 -0600

    Update seeds/index.ts to include the seedFlares function

--- src/server/db/seeds/index.ts
+++ src/server/db/seeds/index.ts
@@ -2,12 +2,24 @@ import seedCategories from './seedCategories';
 import seedInterests from './seedInterests';
 import seedTasks from './seedTasks';
 import seedVenues from './seedVenues';
+import seedFlares from './seedFlares';
+
+/***
+ * Run this file with npm run seed
+ * In order for the seed to work you must
+ *  1. Drop the flare database (if it exists)
+ *  2. Recreate the flare database
+ *  3. Start/Restart the server so the models are read
+ *  4. Create a user on the Flare app
+ * Because the seedEvents script relies on the seed script, you must the seed script first
+ */
 
 async function seedDb() {
   await seedVenues();
   await seedCategories();
   await seedInterests();
   await seedTasks();
+  seedFlares();
 }
 
 seedDb();
