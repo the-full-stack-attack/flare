3895d28696b18f4fef58ac01e9985e1b46e8890e
tymey <tyler.meyer.a@gmail.com>
2025-01-31 20:41:17 -0600

    feat: Adjust POST /api/text to use text from body

--- src/server/routes/text.ts
+++ src/server/routes/text.ts
@@ -8,9 +8,12 @@ const textRouter = Router();
   POST /api/text => Create a text to be sent later for the user
 */
 textRouter.post('/', (req: any, res: Response) => {
-  const { content, send_time, event_id } = req.body;
-  const user_id = req.user.id;
-  Text.create({ content, user_id, send_time, event_id })
+  const { text } = req.body;
+  text.user_id = req.user.id;
+  Text.findOrCreate({
+    where: { user_id: req.user.id, event_id: text.event_id },
+    defaults: text,
+  })
     .then(() => {
       res.sendStatus(200);
     })
