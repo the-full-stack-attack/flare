a3419e1f604da024acaa1dd38f9f13adf716ff08
evan.loria4 <evan.loria4@gmail.com>
2025-02-05 15:02:13 -0600

    Axios req on TaskSidebar to get all user flare tasks

--- src/client/components/tasks/taskSidebar.tsx
+++ src/client/components/tasks/taskSidebar.tsx
@@ -1,30 +1,40 @@
 import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import { UserContext } from '@/client/contexts/UserContext';
+import AchievementCard from './AchievementCard';
 
 type FlareType =
   | {
       id: number;
       name: string;
+      type: string | void;
       icon: string | null;
       achievement: string;
+      milestone: string;
+      description: string;
       value: number;
-      type: string | void;
     }
   | {};
 
 function TaskSidebar() {
   const { user, getUser } = useContext(UserContext);
-  const [flares, setFlare] = useState<FlareType>({});
+  const [taskFlares, setTaskFlares] = useState<FlareType>({});
+
+  useEffect(() => {
+    // GET req to /api/flares/user-flares with a body to tell the server which type of flare
+    axios.get('/api/flares/user-flares')
+      .then(({ data }) => {
+        setTaskFlares(data);
+      })
+      .catch((err) => {
+        console.error('Error getting user task flares:', err);
+      })
+  }, [])
 
   return (
     <div>
-      <div>
-        <p>{`This week: ${user.last_week_task_count} tasks last week`}</p>
-      </div>
-      <div>
-        <p>{`Last week: ${user.weekly_task_count}`}</p>
-      </div>
+      <div className="text-2xl">{`Last week task count: ${user.last_week_task_count}`}</div>
+      <div className="text-2xl">{`This week task count: ${user.weekly_task_count}`}</div>
     </div>
   );
 }
