458c0fac9d251654382a8d4144d9b4bdf88c0701
tymey <tyler.meyer.a@gmail.com>
2025-02-04 22:38:42 -0600

    test: Migrate the teardown tasks for tests to separate file

---
+++ tests/teardown.ts
@@ -0,0 +1,25 @@
+import http from 'http';
+
+export default async function () {
+  function promiseServerClose(server: http.Server): Promise<void> {
+    return new Promise<void>((resolve) => {
+      server.close(() => {
+        resolve();
+      })
+    });
+  }
+
+  try {
+    // Destroy the test user and test event and test venue
+    globalThis.user1.destroy();
+    globalThis.event1.destroy();
+    globalThis.venue1.destroy();
+    globalThis.user2.destroy();
+    globalThis.event2.destroy();
+    globalThis.venue2.destroy();
+    // Close the server
+    await promiseServerClose(globalThis.testServer);
+  } catch (error: unknown) {
+    console.error('Failed to delete test data from DB and close server:', error);
+  }
+}
