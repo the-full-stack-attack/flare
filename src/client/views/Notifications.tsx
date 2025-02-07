2d5038626f3b6fbf165cec018ac7f55672641e13
tymey <tyler.meyer.a@gmail.com>
2025-02-06 19:53:01 -0600

    style: Pass buttonType="Button" to TTSButton

--- src/client/views/Notifications.tsx
+++ src/client/views/Notifications.tsx
@@ -7,6 +7,7 @@ import { UserContext } from '../contexts/UserContext';
 
 import { BackgroundGlow } from '@/components/ui/background-glow';
 
+import TTSButton from '../components/a11y/TTSButton';
 import NotificationList from '../components/notifications-view/NotificationList';
 
 function Notifications() {
@@ -55,9 +56,23 @@ function Notifications() {
     <div className="min-h-screen bg-gradient-to-br from-black via-gray-900 to-pink-900 relative overflow-hidden pt-20 pb-12">
       <BackgroundGlow className="absolute inset-0 z-0 pointer-events-none" />
       <div className="container flex flex-col items-center">
-        <Button className="mb-5" onClick={deleteAllNotifications}>
-          Clear All Notifications
-        </Button>
+        <div className="grid gap-4 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 w-md">
+          <Button className="mb-5" onClick={deleteAllNotifications}>
+            Clear All Notifications
+          </Button>
+          <TTSButton
+            buttonType="Button"
+            text={`Back off, I'll take you on
+Headstrong to take on anyone
+I know that you are wrong
+Headstrong, we're Headstrong
+Back off, I'll take you on
+Headstrong to take on anyone
+I know that you are wrong
+And this is not where you belong`}
+            buttonName="Headstrong"
+          />
+        </div>
         <NotificationList notifs={notifs} getNotifications={getNotifications} />
         {notifs.length === 0 ? (
           <p>You currently have no notifications...</p>
