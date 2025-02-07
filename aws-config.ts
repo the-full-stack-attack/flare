d76e163b959701798bde97a8d90efc175cf89962
evan.loria4 <evan.loria4@gmail.com>
2025-02-07 15:31:52 -0600

    Change aws-config file

--- aws-config.ts
+++ aws-config.ts
@@ -1,24 +1,27 @@
-import AWS from 'aws-sdk';
+import { S3Client } from '@aws-sdk/client-s3';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// These credetials will not be in EC2 .env file
-const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, NODE_ENV, } = process.env
+// These credentials will not be in EC2 .env file
+const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, NODE_ENV } =
+  process.env;
 
-let s3: AWS.S3;
+let s3Client: S3Client = new S3Client({ region: AWS_REGION });
 
 // Because I setup an IAM Role for the EC2 Instance, the deployed site does not need the credentials
-if (NODE_ENV === 'production') {
-  // Use IAM Role in production
-  s3 = new AWS.S3();
-} else {
-  // Use explicit credentials in development
-  s3 = new AWS.S3({
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-    region: AWS_REGION,
-  });
+if (NODE_ENV === 'development') {
+  if (!AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
+    console.error('AWS Credentials not provided from .env');
+  } else {
+    // Use explicit credentials in development
+    s3Client = new S3Client({
+      region: AWS_REGION,
+      credentials: {
+        accessKeyId: AWS_ACCESS_KEY_ID,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY,
+      },
+    });
+  }
 }
-
-export default s3;
+export default s3Client;
