9ae651f30a8680b363726e6f636f52d1a9b5139d
evan.loria4 <evan.loria4@gmail.com>
2025-02-08 14:46:36 -0600

    Create helper to retrieve image url from bucket

--- src/server/helpers/flares.ts
+++ src/server/helpers/flares.ts
@@ -1,9 +1,16 @@
-import User from '../db/models/users';
+import s3Client from '../../../aws-config';
+import { GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import dotenv from 'dotenv';
 import Notification from '../db/models/notifications';
 import Flare from '../db/models/flares';
 import User_Flare from '../db/models/users_flares';
 import User_Notification from '../db/models/users_notifications';
 import Conversation_Session from '../db/models/conversation_session';
+
+dotenv.config();
+
+const { S3_BUCKET_NAME } = process.env;
 
 // This helper will be used to send users flare notifications
 // If we know the name use sendFlareByName(flareName)
@@ -31,7 +38,7 @@ type FlareType = {
 type UseFlareType = {
   UserId: number;
   FlareId: number;
-}
+};
 /*** Function checks if flares need to be sent and sends them if necessary
  * I: Optional String of the flare name to be sent, user object
  * O: n/a
@@ -51,17 +58,22 @@ async function checkForFlares(user: UserType, flareName: string | void) {
   }
   if (user.total_tasks_completed === 1) {
     sendFlareByName('Go Getter');
-  } else if (user.total_tasks_completed % 5 === 0 && user.total_tasks_completed !== 0 ) {
+  } else if (
+    user.total_tasks_completed % 5 === 0 &&
+    user.total_tasks_completed !== 0
+  ) {
     const tasksCompleted = user.total_tasks_completed;
     findFlare('Task Flare', tasksCompleted, user);
   }
   try {
-    const savedConversations = await Conversation_Session.findAll( { where: { user_id: id } });
+    const savedConversations = await Conversation_Session.findAll({
+      where: { user_id: id },
+    });
     if (savedConversations.length === 3) {
       sendFlareByName('Stored Thoughts(x3)');
     }
   } catch (err) {
-    console.error ('Error finding Conversation_Sessions for user: ', err);
+    console.error('Error finding Conversation_Sessions for user: ', err);
     return;
   }
   // Helper Function for if we know the name of the flare to send before the object is received
@@ -75,11 +87,16 @@ async function checkForFlares(user: UserType, flareName: string | void) {
         sendFlareObject(flareToSend, user);
         return;
       } else {
-        console.error('Could not find the flare using the name in sendFlare helper');
+        console.error(
+          'Could not find the flare using the name in sendFlare helper'
+        );
         return;
       }
     } catch (err) {
-      console.error('Error when trying to send the flare in sendFlare helper: ', err);
+      console.error(
+        'Error when trying to send the flare in sendFlare helper: ',
+        err
+      );
     }
   }
 }
@@ -117,7 +134,7 @@ async function sendFlareObject(flare: FlareType, user: UserType) {
     const userId = user.id;
     if (flare) {
       // Create user_flare row
-     await User_Flare.create({
+      await User_Flare.create({
         UserId: userId,
         FlareId: flareId,
       });
@@ -137,16 +154,24 @@ async function sendFlareObject(flare: FlareType, user: UserType) {
         where: { UserId: userId, NotificationId: notificationId },
       });
     } else {
-      console.error('Could not find the flare using the name in sendFlare helper');
+      console.error(
+        'Could not find the flare using the name in sendFlare helper'
+      );
       return;
     }
   } catch (err) {
-    console.error('Error when trying to send the flare in sendFlare helper: ', err);
+    console.error(
+      'Error when trying to send the flare in sendFlare helper: ',
+      err
+    );
   }
 }
 
 // Function makes sure the user doesn't already have the flare
-function alreadyGiven(flare: FlareType, user: UserType): Promise<boolean | void> {
+function alreadyGiven(
+  flare: FlareType,
+  user: UserType
+): Promise<boolean | void> {
   const flareId = flare.id;
   const userId = user.id;
   return User_Flare.findOne({
@@ -164,4 +189,20 @@ function alreadyGiven(flare: FlareType, user: UserType): Promise<boolean | void>
     });
 }
 
-export default checkForFlares;
+// Helper to retrieve the Flare image from the bucket
+const getImageUrl = async (imageKey: string): Promise<string> => {
+  // Create the command with the imageKey
+  const getCommand = new GetObjectCommand({
+    Bucket: S3_BUCKET_NAME,
+    Key: imageKey,
+  });
+  try {
+    const url: string = await getSignedUrl(s3Client, getCommand);
+    return url;
+  } catch (err) {
+    console.error('Error in getImage helper for s3 bucket: ', err);
+    throw err;
+  }
+};
+
+export { checkForFlares, getImageUrl };
