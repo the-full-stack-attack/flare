f5ffedc3a166edf8b55dfe01cb476f268167d205
coltongraygg <coltongraygg@gmail.com>
2025-02-08 11:56:06 -0600

    Avatar renders to page

--- src/client/components/signup/avatar.tsx
+++ src/client/components/signup/avatar.tsx
@@ -1,67 +1,90 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createAvatar } from '@dicebear/core';
 import { adventurer } from '@dicebear/collection';
 
-
 function Avatar() {
+  const [avatarUri, setAvatarUri] = useState('');
+
   const [avatarItems, setAvatarItems] = useState({
-    seed: '',
-    skinColor: '',
-    hair: '',
-    hairColor: '',
-    eyebrows: '',
-    eyes: '',
-    mouth: '',
-    features: '',
-    glasses: '',
-    earrings: '',
+    seed: 'Felix',
+    backgroundColor: '#FFFFFF',
+    radius: 50,
+    hair: 'long01',
+    hairColor: ['0e0e0e'],
+    eyes: 'variant01',
+    mouth: 'variant01',
+    skinColor: ['9e5622'],
+    eyebrows: ['variant01'],
+    features: ['mustache'],
+    glasses: ['variant01'],
+    earrings: ['variant01']
   });
 
-
-  const updateAvatar = async () => {
-
-  }
   const avatarOptions = {
-    seed: [ 'Felix', 'Aneka', ],
-    skinColor: [ '9e5622', '763900', 'ecad80', 'f2d3b1', ],
-    hair: [ 'long01', 'long02', 'long03', 'long04', 'long05', 'long06', 'long07', 'long08', 'long09', 'long10', 'long11', 'long12', 'long13', 'long14', 'long15', 'long16', 'long17', 'long18', 'long19', 'long20', 'long21', 'long22', 'long23', 'long24', 'long25', 'long26',
-            'short01', 'short02', 'short03', 'short04', 'short05', 'short06', 'short07', 'short08', 'short09', 'short10', 'short11', 'short12', 'short13', 'short14', 'short15', 'short16', 'short17', 'short18', 'short19', ],
-    hairColor: [ '0e0e0e', '3eac2c', '6a4e35', '85c2c6', '796a45', '562306', '592454', 'ab2a18', 'ac6511', 'afafaf', 'b9a05f', 'cb6820', 'dba3be', 'e5d7a3', ],
-    eyebrows: [ 'variant01', 'variant02', 'variant03', 'variant04', 'variant05', 'variant06', 'variant07', 'variant08', 'variant09', 'variant10', 'variant11', 'variant12', 'variant13', 'variant14', 'variant15', ],
-    eyes: [ 'variant01', 'variant02', 'variant03', 'variant04', 'variant05', 'variant06', 'variant07', 'variant08', 'variant09', 'variant10', 'variant11', 'variant12', 'variant13', 'variant14', 'variant15', 'variant16', 'variant17', 'variant18', 'variant19', 'variant20', 'variant21', 'variant22', 'variant23', 'variant24', 'variant25', 'variant26', ],
-    mouth: [ 'variant01', 'variant02', 'variant03', 'variant04', 'variant05', 'variant06', 'variant07', 'variant08', 'variant09', 'variant10', 'variant11', 'variant12', 'variant13', 'variant14', 'variant15', 'variant16', 'variant17', 'variant18', 'variant19', 'variant20', 'variant21', 'variant22', 'variant23', 'variant24', 'variant25', 'variant26', 'variant27', 'variant28', 'variant29', 'variant30', ],
-    features: [ 'birthmark', 'blush', 'freckles', 'mustache', ],
-    glasses: [ 'variant01', 'variant02', 'variant03', 'variant04', 'variant05', ],
-    earrings: [ 'variant01', 'variant02', 'variant03', 'variant04', 'variant05', 'variant06', ],
-  }
+    seed: ['Felix', 'Aneka'],
+    skinColor: ['9e5622', '763900', 'ecad80', 'f2d3b1'],
+    hair: [
+      'long01', 'long02', 'long03', 'long04', 'long05', 'long06', 'long07',
+      'long08', 'long09', 'long10', 'long11', 'long12', 'long13', 'long14',
+      'long15', 'long16', 'long17', 'long18', 'long19', 'long20', 'long21',
+      'long22', 'long23', 'long24', 'long25', 'long26',
+      'short01', 'short02', 'short03', 'short04', 'short05', 'short06',
+      'short07', 'short08', 'short09', 'short10', 'short11', 'short12',
+      'short13', 'short14', 'short15', 'short16', 'short17', 'short18', 'short19'
+    ],
+    hairColor: [
+      '0e0e0e', '3eac2c', '6a4e35', '85c2c6', '796a45', '562306',
+      '592454', 'ab2a18', 'ac6511', 'afafaf', 'b9a05f', 'cb6820',
+      'dba3be', 'e5d7a3'
+    ],
+    eyebrows: [
+      'variant01', 'variant02', 'variant03', 'variant04', 'variant05',
+      'variant06', 'variant07', 'variant08', 'variant09', 'variant10',
+      'variant11', 'variant12', 'variant13', 'variant14', 'variant15'
+    ],
+    eyes: [
+      'variant01', 'variant02', 'variant03', 'variant04', 'variant05',
+      'variant06', 'variant07', 'variant08', 'variant09', 'variant10',
+      'variant11', 'variant12', 'variant13', 'variant14', 'variant15',
+      'variant16', 'variant17', 'variant18', 'variant19', 'variant20',
+      'variant21', 'variant22', 'variant23', 'variant24', 'variant25',
+      'variant26'
+    ],
+    mouth: [
+      'variant01', 'variant02', 'variant03', 'variant04', 'variant05',
+      'variant06', 'variant07', 'variant08', 'variant09', 'variant10',
+      'variant11', 'variant12', 'variant13', 'variant14', 'variant15',
+      'variant16', 'variant17', 'variant18', 'variant19', 'variant20',
+      'variant21', 'variant22', 'variant23', 'variant24', 'variant25',
+      'variant26', 'variant27', 'variant28', 'variant29', 'variant30'
+    ],
+    features: ['birthmark', 'blush', 'freckles', 'mustache'],
+    glasses: ['variant01', 'variant02', 'variant03', 'variant04', 'variant05'],
+    earrings: ['variant01', 'variant02', 'variant03', 'variant04', 'variant05', 'variant06']
+  };
 
-  const newAvatar = async () => {
-    const avatar = createAvatar(adventurer, {
-      seed: 'Felix',
-      skinColor: '9e5622',
-      hair: 'long01',
-      hairColor: '0e0e0e',
-      eyebrows: 'variant01',
-      eyes: 'variant01',
-      mouth: 'variant01',
-      features: 'mustache',
-      glasses: 'variant01',
-      earrings: 'variant01',
-    })
-  }
+  const updateAvatar = () => {
+    try {
+      const avatar = createAvatar(adventurer, avatarItems);
+      const uri = avatar.toDataUri();
+      setAvatarUri(uri);
+    } catch (error) {
+      console.error('Error creating avatar:', error);
+    }
+  };
 
+  useEffect(() => {
+    updateAvatar();
+    console.log(avatarUri)
+  }, [avatarItems]);
 
   return (
-      <div className='py-5'>
-        Hello!
-
-
+      <div className="py-5">
+        <img
+            src={avatarUri}
+        />
       </div>
-  )
-
-
-
+  );
 }
 
 export default Avatar;
