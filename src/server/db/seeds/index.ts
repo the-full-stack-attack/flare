b75b2721ce8d1d406e075a000eaeff249f699f36
evan.loria4 <evan.loria4@gmail.com>
2025-02-08 19:17:30 -0600

    Add seedFlares back to full db seed function

--- src/server/db/seeds/index.ts
+++ src/server/db/seeds/index.ts
@@ -2,8 +2,8 @@ import database from '../index';
 import '../models/index'
 import seedCategories from './seedCategories';
 import seedInterests from './seedInterests';
-import seedTasks from './seedTasks';
 import seedVenues from './seedVenues';
+import seedFlares from './seedFlares';
 
 /***
  * Run this file with npm run seed
@@ -19,7 +19,7 @@ async function seedDb() {
   await seedVenues();
   await seedCategories();
   await seedInterests();
-  // await seedTasks();
+  await seedFlares();
 }
 
 seedDb();
