e09990136be9437f56260cdd42f1df591683dbba
bkhebert <bondman2004tippy@gmail.com>
2025-02-17 13:08:14 -0600

    ADD maximum character length per message

--- src/client/views/Chatroom.tsx
+++ src/client/views/Chatroom.tsx
@@ -463,13 +463,25 @@ function Chatroom() {
           </div>
           <div className="flex justify-center mt-2">
         <div onClick={typing}>
-          <Label className="flex justify-center text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 via-orange-500 to-pink-500 text-2xl rounded-md"> Send A Chat </Label>
+          <Label className="flex justify-center  text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 via-orange-500 to-pink-500 text-2xl rounded-md"> 
+            Send A Chat 
+            </Label>
+            <div className="flex justify-center">
           <Textarea 
-            className="bg-white rounded-md w-64" 
+            className="justify-center items-center bg-white rounded-md w-64" 
             type="text"
             value={message}
+            maxlength="150"
             onChange={(e) => setMessage(e.target.value)}
             />
+            </div>
+            <h1 className="flex justify-center items-start mt-2 text-xs text-slate-400">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5 mr-1.5">
+        <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clip-rule="evenodd" />
+      </svg>  
+      Maximum characters per message {message.length} / 150
+    </h1>  
+            {/* <em className="text-white">Allowed Characters</em> */}
           <div className="flex justify-center mt-2">
             <InteractiveHoverButton onClick={sendMessage}>
               Send
