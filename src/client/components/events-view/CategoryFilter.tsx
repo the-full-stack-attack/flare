1dcc3353e20e66cea6950b41ec619c7f3d71fcf0
tymey <tyler.meyer.a@gmail.com>
2025-02-17 15:14:43 -0600

    feat: List out all categories from DB on mount

--- src/client/components/events-view/CategoryFilter.tsx
+++ src/client/components/events-view/CategoryFilter.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import axios from 'axios';
+
+type Category = {
+  name: string;
+  id: number;
+};
 
 function CategoryFilter() {
+  const [catList, setCatList] = useState<Category[]>([]);
+
+  const [changeCatFilter, setChangeCatFilter] = useState<boolean>(false);
+
+  const getCategories = useCallback(() => {
+    axios.get('/api/event/categories')
+      .then(({ data }) => {
+        setCatList(data);
+      })
+      .catch((error: unknown) => {
+        console.error('Failed to getCategories:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   return (
-    <div className="text-gray-200">
-      Division Filter
+    <div className="container mx-auto px-4 mt-4">
+      <p className="text-gray-200">
+        Event Category:
+      </p>
+      {
+        catList.map((cat) => (
+          <div className="text-gray-200">{cat.name}</div>
+        ))
+      }
     </div>
   );
 }
