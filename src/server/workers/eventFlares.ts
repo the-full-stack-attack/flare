75df689b5cae970ba75785f9f563fb1c240affa2
evan.loria4 <evan.loria4@gmail.com>
2025-02-21 09:58:41 -0600

    Start eventFlares helper

---
+++ src/server/workers/eventFlares.ts
@@ -0,0 +1,28 @@
+import { Op } from 'sequelize';
+import cron from 'node-cron';
+import User_Event from '../db/models/users_events';
+import Event from '../db/models/events';
+import { findFlare } from '../helpers/flares';
+
+/***
+ * This worker will be in charge of checking to see if a user has earned an event flare
+ * Steps:
+ * => See what events are occuring and grab the ids
+ * => Use the event id to grab the users attending those events
+ * => Check how many events that user has attended before
+ */
+
+cron.schedule('*/30 * * * *', async () => {
+  // Create a Date to query by
+  const now: number = Date.now();
+  // Find all the events between now and 45 minutes ago
+  const events: any = await Event.findAll({
+    where: {
+      start_time: {
+        [Op.between]: [new Date(now - 1000 * 45 * 10), new Date(now)],
+      },
+    },
+  });
+  // Use the events id to find the users attending
+  // Check how many events the user has attended in the past
+});
