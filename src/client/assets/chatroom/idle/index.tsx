f6420a2a2972a5cb88968f7bb836efa9d8cfc81a
bkhebert <bondman2004tippy@gmail.com>
2025-02-21 12:10:22 -0600

    ADD animated pngs

---
+++ src/client/assets/chatroom/idle/index.tsx
@@ -0,0 +1,9 @@
+const IDLE = {
+  idle1: require('row-1-column-1.png'),
+  idle2: require('row-1-column-2.png'),
+  idle3: require('row-1-column-3.png'),
+  idle4: require('row-1-column-4.png'),
+  idle5: require('row-2-column-1.png'),
+}
+
+export default IDLE;
