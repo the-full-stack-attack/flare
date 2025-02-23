c462b7a1012cc5b40834806c20eeec92fb14c52b
bkhebert <bondman2004tippy@gmail.com>
2025-02-22 20:17:26 -0600

    ADD snap animation

---
+++ src/client/assets/chatroom/snap/index.tsx
@@ -0,0 +1,9 @@
+const SNAP = {
+  105: require('./row-1-column-1 (8).png'),
+  106: require('./row-1-column-2 (5).png'),
+  107: require('./row-1-column-3 (5).png'),
+  108: require('./row-1-column-4 (5).png'),
+  109: require('./row-2-column-1 (4).png'),
+}
+
+export default SNAP;
