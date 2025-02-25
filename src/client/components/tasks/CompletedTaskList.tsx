b03bb05b60cdd7758e4c0158bcf60eb65aa12e34
evan.loria4 <evan.loria4@gmail.com>
2025-02-25 11:35:52 -0600

    Pass filter prop to completedTaskList

--- src/client/components/tasks/CompletedTaskList.tsx
+++ src/client/components/tasks/CompletedTaskList.tsx
@@ -4,6 +4,9 @@ import dayjs from 'dayjs';
 import { UserContext } from '../../contexts/UserContext';
 import CompletedTask from './CompletedTask';
 
+type CompletedTaskListType = {
+  filter: string | null;
+}
 type CompletedTasks = UserTask[];
 type UserTask = {
   completed?: boolean;
@@ -22,21 +25,25 @@ type Task = {
   date: dayjs.Dayjs | '';
   difficulty: number;
 };
-function CompletedTaskList() {
+function CompletedTaskList({ filter }: CompletedTaskListType) {
   const { user } = useContext(UserContext);
   const [completedTasks, setCompletedTasks] = useState<CompletedTasks>([]);
 
   useEffect(() => {
     const { id } = user;
     axios
-      .get(`/api/user_task/complete/${id}`)
+      .get(`/api/user_task/complete/${id}`, {
+        params: {
+          filter,
+        }
+      })
       .then(({ data }) => {
         setCompletedTasks(data);
       })
       .catch((err) => {
         console.error('Error fetching user_tasks: ', err);
       });
-  }, [user]);
+  }, [user, filter]);
   return (
     <div className="relative group transition-all duration-300 hover:transform text-white">
       <div className="absolute inset-0 rounded-2xl bg-gradient-to-br from-white/[0.07] to-white/[0.03] blur" />
