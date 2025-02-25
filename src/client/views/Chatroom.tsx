66dc97667dc80c2700df882cd6d13d127946947f
bkhebert <bondman2004tippy@gmail.com>
2025-02-24 23:12:54 -0600

    ADD dynamically resizing layout for chatroom, format with prettier

--- src/client/views/Chatroom.tsx
+++ src/client/views/Chatroom.tsx
@@ -775,49 +775,7 @@ const [isReady, setIsReady] = useState(false);
           </div>
         )}
         {(isPlayingDJ || onKeyboard) && <DJam eventId={eventId} user={user} />}
-        <div className="flex justify-center mt-2">
-          <div onClick={typing}>
-            <Label className="flex justify-center  text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 via-orange-500 to-pink-500 text-2xl rounded-md">
-              Send A Chat
-            </Label>
-            <div className="flex justify-center">
-              <div className="relative">
-                <Textarea
-                  className="justify-center items-center  border-orange-500  bg-gray-900 text-yellow-500 rounded-md w-72"
-                  type="text"
-                  placeholder="Be kind to each other..."
-                  value={message}
-                  maxlength="150"
-                  onChange={(e) => setMessage(e.target.value)}
-                />
-                <BsSend
-                  onClick={sendMessage}
-                  className="absolute w-5 h-5 bottom-2.5 right-2.5 text-white hover:text-orange-500"
-                />
-                {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="absolute w-5 h-5 top-2.5 right-2.5 text-slate-600"> </svg> */}
-                {/* <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z"></path>
-                 */}
-              </div>
-            </div>
-            <h1 className="flex justify-center items-start mt-2 text-xs text-slate-400">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                class="w-5 h-5 mr-1.5"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <em> Message Limit: {message.length} / 150</em>
-            </h1>
-
-            {/* <em className="text-white">Allowed Characters</em> */}
-          </div>
-        </div>
+     
         {!isPlayingQuiplash && (
           <Card className="w-50 block sm:hidden bg-transparent border-transparent">
             <div className="flex flex-col items-center">
@@ -859,20 +817,74 @@ const [isReady, setIsReady] = useState(false);
           </Card>
         )}
 
-        <Card className="bg-transparent border-transparent flex items-center justify-center">
-          <div className="h-[300px] overflow-y-auto">
-            <AnimatedList className="w-80 md:w-160 lg:w-300">
-              {allMessages.map((msg) => (
-                <MsgBox
-                  className="w-80 md:w-360 lg:w-565"
-                  msg={msg.message}
-                  user={msg.username}
-                  eventId={eventId}
-                />
-              ))}
-            </AnimatedList>
-          </div>
-        </Card>
+
+
+<div>
+
+<div className="block mt-2 p-4 h-96">
+  <Card className="bg-transparent border-transparent">
+    <div onClick={typing}>
+      <Card className="bg-transparent border-transparent flex items-center justify-center">
+        {/* Message Box Container with Responsive Height */}
+        <div className="h-[15vh] md: h-[24] lg:h-[50vh] w-full max-w-7xl overflow-y-auto">
+          <AnimatedList>
+            {allMessages.map((msg) => (
+              <MsgBox
+                className="w-80 md:w-360 lg:w-565"
+                msg={msg.message}
+                user={msg.username}
+                eventId={eventId}
+              />
+            ))}
+          </AnimatedList>
+        </div>
+      </Card>
+
+      {/* Label */}
+      <Label className="flex justify-center text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 via-orange-500 to-pink-500 text-2xl rounded-md">
+        Send A Chat
+      </Label>
+
+      {/* Textarea Input */}
+      <div className="flex justify-center">
+        <div className="relative">
+          <Textarea
+            className="justify-center items-center border-orange-500 bg-gray-900 text-yellow-500 rounded-md w-72"
+            type="text"
+            placeholder="Be kind to each other..."
+            value={message}
+            maxLength="150"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <BsSend
+            onClick={sendMessage}
+            className="absolute w-5 h-5 bottom-2.5 right-2.5 text-white hover:text-orange-500"
+          />
+        </div>
+      </div>
+
+      {/* Message Limit Display */}
+      <h1 className="flex justify-center items-start mt-2 text-xs text-slate-400">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-5 h-5 mr-1.5"
+        >
+          <path
+            fillRule="evenodd"
+            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <em>Message Limit: {message.length} / 150</em>
+      </h1>
+    </div>
+  </Card>
+</div>
+
+       
+        </div>
       </div>
     </div>
   );
