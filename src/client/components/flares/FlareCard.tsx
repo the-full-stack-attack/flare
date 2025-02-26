093753a9395e0ebe95ef647a7951233a48a885c7
evan.loria4 <evan.loria4@gmail.com>
2025-02-26 10:45:07 -0600

    Flare cards are clickable

--- src/client/components/flares/FlareCard.tsx
+++ src/client/components/flares/FlareCard.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { motion } from 'framer-motion';
-import cn from '../../../../lib/utils';
+import {
+  Dialog,
+  DialogTrigger,
+  DialogClose,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+} from '../../../components/ui/dialog';
 
 type FlareCardType = {
   flare: FlareType;
   index: number;
-}
+};
 
 type FlareType = {
   id: number;
@@ -15,18 +21,40 @@ type FlareType = {
   achievement: string;
   milestone: string;
   description: string;
+  notification_message: string;
   value: number;
 };
 
 function FlareCard({ flare, index }: FlareCardType) {
   return (
-  <div className="p-6">
-    <div>
-      <img className="rounded-full" src={flare.icon} />
-      <h3 className="font-bold my-2 bg-gradient-to-r from-yellow-500 via-orange-500 to-pink-500 bg-clip-text text-transparent text-center">{flare.name}</h3>
-    </div>
-  </div>
-  )
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className="p-6">
+            <div>
+              <img className="rounded-full" src={flare.icon} />
+              <h3 className="font-bold my-2 bg-gradient-to-r from-yellow-500 via-orange-500 to-pink-500 bg-clip-text text-transparent text-center">
+                {flare.name}
+              </h3>
+            </div>
+          </div>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogTitle>{flare.name}</DialogTitle>
+          <div className="flex items-start space-x-4">
+            <img className="rounded-full w-24 h-24 object-cover" src={flare.icon} />
+            <DialogDescription className="flex-grow">{flare.notification_message}</DialogDescription>
+          </div>
+          <DialogClose asChild>
+            <button
+              className="px-4 py-2 rounded-lg w-20 text-center bg-gradient-to-r from-purple-500 to-pink-500 text-white font-medium
+                    hover:from-purple-600 hover:to-pink-600 transition-all duration-300 hover:scale-[1.02] active:scale-[0.98] ml-auto"
+            >
+              Close
+            </button>
+          </DialogClose>
+        </DialogContent>
+      </Dialog>
+  );
 }
 
 export default FlareCard;
