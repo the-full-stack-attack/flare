16b798d17bd94a552fd39f3462cb2687364c210a
tymey <tyler.meyer.a@gmail.com>
2025-03-10 20:31:32 -0500

    seed: Add Op Spark Showcase After Party to seedEvents

--- src/server/db/showcase-seeds/seedEvents.ts
+++ src/server/db/showcase-seeds/seedEvents.ts
@@ -55,6 +55,11 @@ const notifications: SeedNotification[] = [
     message: `The upcoming event you're attending, Karaoke Night, starts soon in an hour. Hope to see you there.`,
     send_time: new Date('2025-03-16T23:00:00.000Z'), // 03.16.25 => 6:00 PM
   },
+  { // #2: Op Spark Showcase After Party
+    title: null,
+    message: `The upcoming event you're attending, Op Spark Showcase After Party, starts soon in an hour. Hope to see you there.`,
+    send_time: new Date('2025-03-14T00:00:00.000Z'), // 03.13.25 => 7:00 PM
+  },
 ];
 
 const events: SeedEvent[] = [
@@ -69,12 +74,29 @@ const events: SeedEvent[] = [
     created_by: 1, // Leeroy Jenkins
     hour_before_notif: 1,
   },
+
+  /* #2 Op Spark Showcase After Party */
+  {
+    title: 'Op Spark Showcase After Party',
+    start_time: new Date('2025-03-14T01:00:00.000Z'), // 03.13.25 => 8:00 PM
+    end_time: new Date('2025-03-14T03:00:00.000Z'), // 03.13.25 => 10:00 PM
+    description: `Let's have a drink and celebrate Operation Spark's new graduates.`,
+    venue_id: 2, // Anna's
+    category_id: 1, // Casual Meetups
+    created_by: 2, // John Smith
+    hour_before_notif: 2,
+  },
 ];
 
 const eventInterests: SeedEventInterest[] = [
   /* #1: Karaoke Night */
   { EventId: 1, InterestId: 2 }, // Music
   { EventId: 1, InterestId: 10 }, // Nightlife
+
+  /* #2: Op Spark Showcase After Party */
+  { EventId: 2, InterestId: 7 }, // Technology
+  { EventId: 2, InterestId: 10 }, // Nightlife
+  { EventId: 2, InterestId: 15 }, // Community Events
 ];
 
 const eventVenueImages: SeedEventVenueImage[] = [
@@ -82,6 +104,12 @@ const eventVenueImages: SeedEventVenueImage[] = [
   { display_order: 0, EventId: 1, VenueImageId: 1 },
   { display_order: 1, EventId: 1, VenueImageId: 3 },
   { display_order: 2, EventId: 1, VenueImageId: 6 },
+
+  /* #2 Op Spark Showcase After Party */
+  { display_order: 0, EventId: 2, VenueImageId: 8 },
+  { display_order: 1, EventId: 2, VenueImageId: 12 },
+  { display_order: 2, EventId: 2, VenueImageId: 7 },
+  { display_order: 3, EventId: 2, VenueImageId: 10 },
 ];
 
 const eventVenueTags: SeedEventVenueTag[] = [
@@ -91,11 +119,20 @@ const eventVenueTags: SeedEventVenueTag[] = [
   { display_order: 2, EventId: 1, VenueTagId: 16 }, // Eclectic
   { display_order: 3, EventId: 1, VenueTagId: 20 }, // Sing
   { display_order: 4, EventId: 1, VenueTagId: 21 }, // Atmosphere
+
+  /* #2: Op Spark Showcase After Party */
+  { display_order: 0, EventId: 2, VenueTagId: 33 }, // Cosy Atmosphere
+  { display_order: 1, EventId: 2, VenueTagId: 38 }, // Pool Table
+  { display_order: 2, EventId: 2, VenueTagId: 31 }, // Casual
+  { display_order: 3, EventId: 2, VenueTagId: 32 }, // Neighborhood
 ];
 
 const chatrooms: SeedChatroom[] = [
   /* #1 Karaoke Night */
   { event_id: 1 },
+
+  /* #2 Op Spark Showcase After Party */
+  { event_id: 2 },
 ];
 
 const userEvents: SeedUserEvent[] = [
@@ -103,6 +140,10 @@ const userEvents: SeedUserEvent[] = [
   { UserId: 1, EventId: 1 }, // Leeroy Jenkins
   { UserId: 2, EventId: 1 }, // John Smith
   { UserId: 3, EventId: 1 }, // Susan Bellum
+
+  /* #2 Op Spark Showcase After Party */
+  { UserId: 1, EventId: 1 }, // Leeroy Jenkins
+  { UserId: 2, EventId: 1 }, // John Smith
 ];
 
 const seedEvents = async () => {
